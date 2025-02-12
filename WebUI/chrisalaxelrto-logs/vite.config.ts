038e1dcc497f3feed72127c1d0aaa062a8485472
Alberto Jahuey Moncada <albertojahueymoncada@gmail.com>
2025-02-11 19:23:58 -0800

    Add allowed host

--- WebUI/chrisalaxelrto-logs/vite.config.ts
+++ WebUI/chrisalaxelrto-logs/vite.config.ts
@@ -4,6 +4,12 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: '0.0.0.0', // Allow access from external hosts
+    port: 5173, // Default Vite port
     allowedHosts: ['chrisalaxelrto.porebazu.lat'],
+    hmr: {
+      host: 'chrisalaxelrto.porebazu.lat',
+      port: 5173,
+    },
   },
 });
